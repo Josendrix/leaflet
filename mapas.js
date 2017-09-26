a9c99ac7b139e5a0a720073f34858619e83b1937
Jose Manuel Roco <josendrix@hotmail.com>
2017-09-26 19:49:00 +0200

    3capas-Sin Geolocalizacion-javascript separado final

--- mapas.js
+++ mapas.js
@@ -1,37 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
+var map = L.map('map').
+setView([37.185748, -3.610183],
+15);
 
-	<style>
-	#map { 
-    	width: 100%;
-    	height: 580px;
-    	box-shadow: 5px 5px 5px #888;
- 	}
-
-	</style>
-</head>
-	<body>
-		<div id="map"></div>
-		<script>
-  		var map = L.map('map').
-     		setView([37.185748, -3.610183],
-     		15);
-
-		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var mapLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 maxZoom: 18
 }).addTo(map);
 
- 	
+/*
+map.addLayer(L.tileLayer);
+*/
+
+var googleLayer = new L.Google();
+map.addLayer(googleLayer);
+
+var googleRoadmapLayer = new L.Google('ROADMAP');
+map.addLayer(googleRoadmapLayer);
+
+var baseMaps = {
+	'Google Roadmap' : googleRoadmapLayer,
+	'Google' : googleLayer,
+	'leaflet' : mapLayer
+};
+
+
+L.control.layers(baseMaps,null).addTo(map);
+
+/*
 L.control.scale().addTo(map);
 
-L.marker([37.185748, -3.610183],{draggable: true}).addTo(map);
-		</script>
+map.locate({setView: true, maxZoom: 16});
 
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
 
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-	</body>
-</html>
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+*/
